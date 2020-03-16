9daeab799aa0512662aa944e0db1ce29ce2cf4c7
Schöffer Rudolf <spureszka@gmail.com>
2020-03-16 23:55:15 +0100

    more error handle

--- middlewares/newcontentmw.js
+++ middlewares/newcontentmw.js
@@ -1,6 +1,10 @@
 module.exports = function(objRepo) {
     return function(req,res,next) {
         if(typeof req.body.title !== "undefined"){
+            if(req.body.title === '' || req.body.desc === ''){
+                objRepo.error.push('Hiba: Üresen hagyott mezők!')
+                return res.render('newcontent', {error: objRepo.error});
+            }
             objRepo.Newcontent.create({title: req.body.title, desc: req.body.desc}, function(err, newcontent){
                 if(err){
                     console.log(err);
@@ -11,7 +15,7 @@ module.exports = function(objRepo) {
                 }
             })
         } else {
-            return res.render('newcontent');
+            return res.render('newcontent', {error: objRepo.error});
         }
     }
 }
--- middlewares/newforummw.js
+++ middlewares/newforummw.js
@@ -1,8 +1,12 @@
 module.exports = function(objRepo) {
     return function(req,res,next){
         if(typeof req.body.forumname === "undefined"){
-           return res.render('newforum', {id: req.params.id});
+           return res.render('newforum', {id: req.params.id, error: objRepo.error});
         } else {
+            if(req.body.forumname === ''){
+                objRepo.error.push('Üresen hagytad a mezőt!')
+                return res.render('newforum', {id: req.params.id, error: objRepo.error});
+            }
             objRepo.Newcontent.findById(req.params.id, function(err, newcontent){
                 if(err){
                     console.log(err);
